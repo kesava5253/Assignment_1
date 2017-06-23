b42911bd7c6112868ee49d93b44481630fa45676
kesava5253 <ratnakesavareddy@gmail.com>
2017-06-23 20:19:10 +0530

    updated cpu usage with user input time period

--- cpu.sh
+++ cpu.sh
@@ -1,11 +1,26 @@
 #! /bin/bash
 
+if [[ $# -lt 1 ]];
+then       
+	echo "usage:$0 <missing argument>"
+	echo "input time period in secs"
+	exit 102
+fi
+
 printf "Memory\t\tDisk\t\tCPU\n"
-end=$((SECONDS+3600))
-while [ $SECONDS -le $end ]; do
-MEMORY=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
-DISK=$(df -h | awk '$NF=="/"{printf "%s\t\t", $5}')
-CPU=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
-echo "$MEMORY$DISK$CPU"
-sleep 5
+	end=$((SECONDS+$1))
+	while [ $SECONDS -le $end ]; do
+		MEMORY=$(free -m | awk 'NR==2{printf "%.2f%%\t\t",$3*100/$2 }')
+		DISK=$(df -h | awk '$NF=="/"{printf "%s\t\t", $5}')
+		CPU=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
+		echo "$MEMORY$DISK$CPU"
+		sleep 5
 done
+
+
+
+
+
+#grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}'
+
+
