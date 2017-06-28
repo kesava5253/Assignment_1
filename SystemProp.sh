85aafb72a55cd990866326e482b77eb9495b2dd3
kesava5253 <ratnakesavareddy@gmail.com>
2017-06-28 15:31:53 +0530

    v-1.4 methods for disk mem cpu

--- SystemProp.sh
+++ SystemProp.sh
@@ -20,41 +20,13 @@ avg2=`echo "scale = 2; $1/$2" | bc`
 echo "Avg disk : $avg2"
 }
 
-let count=max=max1=max2=mem=DiskUsage=CpuUsage=0
+#Method to run cpu - call from main 
+RunCpu(){
 
-
-
-if [[ $# -lt 1 ]];
-then       
-	echo "usage:$0 <missing argument> [cpu/disk/mem]"
-	echo "Argument time period in secs"
-	exit 102
-elif [ $# -eq 2 ];
-then
+printf "Cpu\n"
 
 end=$((SECONDS+$1))
-
-	if [ "$2" == "mem" ];
-	then
-	printf "Memory\n"
-	while [ $SECONDS -le $end ]; do
-	MEMORY=$(free -m | awk 'NR==2{printf "%.2f\t\t",$3*100/$2 }')
-	echo "$MEMORY"
-
-	 if [[ "$MEMORY" > "$max" ]]; then
-	 max="$MEMORY"
-  	fi
-	mem=`echo  $mem + $MEMORY | bc`
-	count=`echo $count + 1 | bc`
-	sleep 5
-	done
-	AvgMemory $mem $count
-	echo "Maximum memory :$max "
-	fi
-
-	if [ "$2" == "cpu" ];
-	then
-	printf "Cpu\n"
+ 
 	while [ $SECONDS -le $end ]; do
  	CPU=$(top -bn1 | grep load | awk '{printf "%.2f\t\t\n", $(NF-2)}')
 	echo "$CPU"
@@ -68,12 +40,39 @@ end=$((SECONDS+$1))
 	sleep 5
 	done
 	AvgCpu $CpuUsage $count
-	echo "Maximum CPU :$max1 "
-	fi
-	
-	if [ "$2" == "disk" ];
-	then
-	printf "Disk\n"
+	echo "Maximum CPU used:$max1 "
+
+}
+
+#Method to run memory call from main 
+RunMemory(){
+
+printf "Memory\n"
+
+end=$((SECONDS+$1))
+   
+	while [ $SECONDS -le $end ]; do
+	MEMORY=$(free -m | awk 'NR==2{printf "%.2f\t\t",$3*100/$2 }')
+	echo "$MEMORY"
+
+	 if [[ "$MEMORY" > "$max" ]]; then
+	 max="$MEMORY"
+  	fi
+	mem=`echo  $mem + $MEMORY | bc`
+	count=`echo $count + 1 | bc`
+	sleep 5
+	done
+	AvgMemory $mem $count
+	echo "Maximum memory used:$max "
+
+}
+
+ #Method to run disk call from main 
+RunDisk(){
+printf "Disk\n"
+
+end=$((SECONDS+$1))
+    
 	while [ $SECONDS -le $end ]; do
 	DISK=$(df -h | awk '$NF=="/"{printf "%.2f\t\t", $5}')
 	echo "$DISK"
@@ -87,11 +86,12 @@ end=$((SECONDS+$1))
 	sleep 5
 	done
 	AvgDisk $DiskUsage $count
-	echo "Maximum DISK :$max2 "
-	fi
-else 
+	echo "Maximum DISK used :$max2 "
 
-end=$((SECONDS+$1))
+}
+RunAll(){
+
+   end=$((SECONDS+$1))
       printf "Memory\t\tDisk\t\tCPU\n"
 	while [ $SECONDS -le $end ]; do
 		MEMORY=$(free -m | awk 'NR==2{printf "%.2f\t\t",$3*100/$2 }')
@@ -130,6 +130,48 @@ AvgCpu $CpuUsage $count
 echo "Maximum memory :$max "
 echo "Maximum CPU :$max1 "
 echo "Maximum DISK :$max2 "
+
+}
+
+
+# main program starts from here
+
+let count=max=max1=max2=mem=DiskUsage=CpuUsage=0
+
+
+
+if [[ $# -lt 1 ]];
+then       
+	echo "usage:$0 <missing argument> [cpu/disk/mem]"
+	echo "Argument time period in secs"
+	exit 102
+elif [ $# -eq 2 ];
+then
+
+
+	if [ "$2" == "mem" ];
+	then
+
+	RunMemory $1 #Divert to method RunMemory
+	
+	fi
+
+	if [ "$2" == "cpu" ];
+	then
+
+	RunCpu $1 #Divert to method RunCpu
+	
+	fi
+	
+	if [ "$2" == "disk" ];
+	then
+
+	RunDisk $1 #Divert to method RunDisk
+	
+	fi
+else  
+
+	RunAll $1 #Divert to method RunAll
 
 fi
 
