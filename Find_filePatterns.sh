29c3b7b8e2391ed35b6e7ede0eb1f3eeafffcb7e
kesava5253 <ratnakesavareddy@gmail.com>
2017-06-28 10:55:38 +0530

    updated for review

--- Find_filePatterns.sh
+++ Find_filePatterns.sh
@@ -1,7 +1,17 @@
 #! /bin/bash
-if [[ $# -ne 2 ]];
+
+
+if [ $# -lt 2 ];
 then       
-	echo "usage:$0 <file type> <pattern>"
+	echo "usage:$0 <file type> <'pattern\|pattern'>"
 	exit 102
-fi 
+fi
+
+if [ $# -eq 2 ];
+then
 find *.$1|xargs grep $2
+
+else
+
+find *.$1 |xargs grep -i $2
+fi
--- Listfile.sh
+++ Listfile.sh
@@ -1,15 +1,44 @@
 #! bin/bash
 
+if [ $# -lt 1 ];
+then       
+	echo "usage:$0 <missing argument> [optional]"
+	echo "type command as argument"
+	exit 102
+fi
+if [ $# -gt 3 ];
+then
+echo "Usage $0 : <command> [extension command] [directory]"
+echo "maximum 3 commands arguments allowed"
+exit 1
+fi
+
 echo "Username"
 read user_name
 
 echo "hostname"
 read Host
 
-script="ls"
+if [ $# -eq 1 ];
+then
+script=$1
 
 ssh -X $user_name@$Host "${script}"
 
+elif [ $# -eq 2 ];
+then
+script=$1" "$2
 
-done
+ssh -X $user_name@$Host "${script}"
+
+else
+direc="/home/rihu/"$3
+script=$1" "$direc"/"
+
+ssh -X $user_name@$Host "${script}"
+
+fi
+
+
+#password as input pending
 
